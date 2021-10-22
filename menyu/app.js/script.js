d6cb8e884000eaece9caae640c82b88dcfdc58bd
nursulton13 <nursultonahmadov65@gmail.com>
2021-10-22 21:03:37 +0500

    safiya-bakery-copiya website commit

---
+++ menyu/app.js/script.js
@@ -0,0 +1,11 @@
+var x =  document.getElementById("screen-menu");
+var y = document.getElementById("body1");
+
+function button(params) {
+   if (x.style.display === "none"){
+       x.style.display = "block";
+   } 
+   else{
+        x.style.display = "none";
+   }
+}
